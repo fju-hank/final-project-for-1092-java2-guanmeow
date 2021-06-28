a947b3478334d70d8a4a8f4fedb24575c4cd4cfa
GM <nicoyazawa0501@gmail.com>
2021-06-28 17:49:05 +0800

    在修改字

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -9,24 +9,25 @@ public class Tester {
         Kid kid = new Kid();
         Elder elder = new Elder();
         Ticket ticket = new Ticket();
-        System.out.println("歡迎光臨本售票機，進入新竹市親山步道前必須購買入場券");
-        System.out.println("請問您要購買還是閱讀親山步道資訊?");
-        System.out.println("按a開啟步道資訊再開始購票;按b直接開始購票");
-        String s = sc.next();
-        if(s.equals("a")){
-            ticket.rule();
 
-        }else if(s.equals("b")) {
-        }
+            System.out.println("歡迎光臨本售票機，進入新竹市親山步道前必須購買入場券");
+            System.out.println("請問您要購買入場券還是閱讀親山步道資訊?");
+            System.out.println("按a開啟步道資訊再開始購票;按b直接開始購票");
+            String s = sc.next();
+            if (s.equals("a")) {
+                ticket.rule();
+
+            } else if (s.equals("b")) {
+            }
             System.out.println("購票規則:");
             System.out.println("身高150cm以下算小孩");
             System.out.println("老人票必須超過65歲");
             System.out.println("團體打九折活動須單次購買加總超過10張(不限票型)");
-            System.out.println("請問您要幾張大人票("+ adult.cost+ "元/張)?");
+            System.out.println("請問您要幾張大人票(" + adult.cost + "元/張)?");
             int a = sc.nextInt();
-            System.out.println("請問您要幾張孩童票("+ kid.cost+ "元/張)");
+            System.out.println("請問您要幾張孩童票(" + kid.cost + "元/張)");
             int k = sc.nextInt();
-            System.out.println("請問您要幾張敬老票("+ elder.cost+ "元/張)");
+            System.out.println("請問您要幾張敬老票(" + elder.cost + "元/張)");
             int e = sc.nextInt();
             int total = a * adult.cost + k * kid.cost + e * elder.cost;
             float all = 0;
@@ -56,4 +57,5 @@ public class Tester {
                     + ten + "枚10元, " + five + "枚5元, " + one + "枚1元");
             System.out.println("謝謝購買");
         }
+
 }
